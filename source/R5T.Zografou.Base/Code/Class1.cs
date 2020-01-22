0d7afb9097e715d5ed2b89981f36608dda9e5fb1
MinexAutomation <minex.automation@gmail.com>
2020-01-22 00:08:48 -0800

    Added ITestingDataDirectoryNameConvention service definition interface type.

--- source/R5T.Zografou.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Zografou.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Zografou.Base/Code/Services/Definitions/ITestingDataDirectoryNameConvention.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Zografou
+{
+    public interface ITestingDataDirectoryNameConvention
+    {
+        string GetTestingDataDirectoryName();
+    }
+}
