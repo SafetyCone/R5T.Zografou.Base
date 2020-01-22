4b6b11816f161a0f622bb45a120601732a5c0b96
MinexAutomation <minex.automation@gmail.com>
2020-01-22 00:06:53 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Zografou.Base.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Zografou.Base.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
