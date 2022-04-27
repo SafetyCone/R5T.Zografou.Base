861fa652f4e58a1cd2e9a76649c69604dcdef494
davidcoats <david@getsafetycone.com>
2022-04-27 15:37:57 -0700

    Interim changes.

--- source/R5T.Zografou.Base/Code/Services/Definitions/ITestingDataDirectoryNameConvention.cs
+++ source/R5T.Zografou.Base/Code/Services/Definitions/ITestingDataDirectoryNameConvention.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Zografou
-{
-    public interface ITestingDataDirectoryNameConvention
+{[ServiceDefinitionMarker]
+    public interface ITestingDataDirectoryNameConvention:IServiceDefinition
     {
         string GetTestingDataDirectoryName();
     }
